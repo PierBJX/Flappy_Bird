ff14ec80c390b3561e346e7563f27ed48a6c9e23
Biojoux Pierre <pierre.biojoux@ge.com>
2019-12-19 00:09:41 +0100

    Add tapController.cs

--- Assets/Scripts/TapController.cs
+++ Assets/Scripts/TapController.cs
@@ -2,17 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class TapController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float tapForce = 10;
+    public float tiltSmooth = 5;
+    public Vector3 startPos;
+
+    Rigidbody2D rigidbody;
+    Quaternion downRotation;
+    Quaternion forwardRotation;
+
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
+        downRotation = Quaternion.Euler(0, 0, -90);
+        forwardRotation = Quaternion.Euler(0, 0, 35);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            transform.rotation = forwardRotation;
+            rigidbody.velocity = Vector3.zero;
+            rigidbody.AddForce(Vector2.up * tapForce, ForceMode2D.Force);
+        }
+        transform.rotation = Quaternion.Lerp(transform.rotation, downRotation, tiltSmooth * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "ScoreZone")
+        {
+            // Register a score event
+            // Play a sound
+        }
+
+        if (col.gameObject.tag == "DeadZone")
+        {
+            rigidbody.simulated = false;
+            // Register a dead event
+            // Play a sound
+        }
     }
 }
